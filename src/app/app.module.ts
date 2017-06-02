17e380e78e9be53ce3e3a3f150e1e61670c49ae5
Amit Papushe Shely <papushe9@gmail.com>
2017-06-02 14:48:07 +0300

    delete - rightNav, logo. change position - arrivel, mix-tape, about, explore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,20 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { MixTapeComponent } from './header/right-nav/mix-tape/mix-tape.component';
-import { RightNavComponent } from './header/right-nav/right-nav.component';
-import { ArrivelComponent } from './header/right-nav/arrivel/arrivel.component';
-import { AboutComponent } from './header/right-nav/about/about.component';
-import { ExploreComponent } from './header/right-nav/explore/explore.component';
-import { LogoComponent } from './header/logo/logo.component';
+import { MixTapeComponent } from './mix-tape/mix-tape.component';
+import { ArrivelComponent } from './arrivel/arrivel.component';
+import { AboutComponent } from './about/about.component';
+import { ExploreComponent } from './explore/explore.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MixTapeComponent,
-    RightNavComponent,
     ArrivelComponent,
     AboutComponent,
-    ExploreComponent,
-    LogoComponent
+    ExploreComponent
   ],
   imports: [
     BrowserModule,
