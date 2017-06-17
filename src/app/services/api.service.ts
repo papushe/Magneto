17904d39bcb95a7c7ea3d9488da996e89da2478c
Amit Papushe Shely <papushe9@gmail.com>
2017-06-17 15:09:44 +0300

    arrival, mix-tape

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -43,7 +43,7 @@ export class ApiService {
     let minutes: any, seconds: any;
     minutes = Math.floor((ms % 3600000) / 60000); // 1 Minutes = 60000 Milliseconds
     seconds = Math.floor(((ms % 360000) % 60000) / 1000); // 1 Second = 1000 Milliseconds
-    seconds = (seconds < 10)? `0 + ${seconds}` : seconds;
+    seconds = (seconds < 10)? `0${seconds}` : seconds;
     return  minutes + ':' + seconds;
   }
 }
