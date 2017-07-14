2e2284afcfceea695140a6952f278d968f233cdb
NaorHai <Na1994or>
2017-07-14 16:09:07 +0300

    integration with voices and forms

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, Renderer, Input, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
+  host: {
+    '(window:keydown)': 'handleKeyboardEvents($event)'
+  }
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements AfterViewInit {
+  @Input() src: string;
+  @ViewChild('player') player;
+
+  baseUrl:string = '../../assets/sounds/';
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {}
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeyboardEvent() {
+    this.getRandomSound();
+    this.playSound();
   }
 
+  getRandomSound() {
+    let whichSound  = Math.floor(Math.random() * (8)) + 1;
+    this.src =  `${this.baseUrl}${whichSound}.mp3`;
+  }
+
+  playSound() {
+    let playPromise = this.player.nativeElement.play();
+
+    if (playPromise !== undefined) {
+      playPromise.then(_ => {
+
+      }).catch(error => {});
+    }
+  }
 }
