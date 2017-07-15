2b7e436211ab82e0396ad76dff24ab5e6dd804c1
Amit Papushe Shely <papushe9@gmail.com>
2017-07-15 18:38:02 +0300

    create mix - working delete mix - working

--- src/app/explore/explore.component.ts
+++ src/app/explore/explore.component.ts
@@ -24,15 +24,15 @@ export class ExploreComponent implements OnInit {
       .then()
   }
   onDelete(val) {
-    this.deletedMix(val.mixName);
-    this.alertDeleted=`Success: <i>${val.mixName}</i> was Deleted`;
+    this.deletedMix(val.deleteMixName);
+    this.alertDeleted=`Success: <i>${val.deleteMixName}</i> was Deleted`;
   }
   private createMix(name, creator, track1, track2, track3){
     this.apiService.createMix(name, creator, track1, track2, track3)
       .then()
   }
   onCreate(val){
-    this.createMix(val.mixName,val.creator,val.trackId1,val.trackId2,val.trackId3);
-    this.alertCreated=`Success: <i>${val.mixName}</i> was Created`;
+    this.createMix(val.createMixName,val.creator,val.trackId1,val.trackId2,val.trackId3);
+    this.alertCreated=`Success: <i>${val.createMixName}</i> was Created`;
   }
 }
